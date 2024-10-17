ed60c361ebded63007211bd2f854fc7627e6c47d
wilson reis <wilson.wreis@gmail.com>
2024-10-17 13:53:23 -0300

    Primeiro commit

---
+++ src/main/java/br/com/wilson/camel/BookRoute.java
@@ -0,0 +1,24 @@
+package br.com.wilson.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        restConfiguration()
+                .component("servlet")
+                .bindingMode(RestBindingMode.json);
+        rest("/api")
+                .get("/hello")
+                .to("direct:hello");
+
+        from("direct:hello")
+                .log("Received request for /api/hello")
+                .setBody(constant("Hello, World!"));
+    }
+}
